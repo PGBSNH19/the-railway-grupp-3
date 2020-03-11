f90603dd260425baff6129b0b4cd94707e3c98cd
Eric Sandberg <56867936+kaptenen@users.noreply.github.com>
2020-03-11 14:51:21 +0100

    @kaptenen

---
+++ Source/TrainConsole/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainConsole
+{
+   public static class Data
+    {
+		public static void LoadAllFiles() { }
+
+		private int myVar;
+
+		public int MyProperty
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+	}
+}
