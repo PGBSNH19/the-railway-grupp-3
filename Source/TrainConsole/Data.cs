58a8874afd2f213e1e90124dbe1d5137666bac58
IzC87 <fredrikoden87@gmail.com>
2020-03-16 13:08:26 +0100

    @izc87 Added method to get the Separator

--- Source/TrainConsole/Data.cs
+++ Source/TrainConsole/Data.cs
@@ -5,60 +5,89 @@ using System.IO;
 
 namespace TrainConsole
 {
-   public class Data
+    public class Data
     {
-		public List<Track> Tracks = new List<Track>();
-		public List<TrackSwitch> TrackSwitches = new List<TrackSwitch>();
-		public List<Station> Stations = new List<Station>();
-		public List<Train> Trains = new List<Train>();
-		public List<Gate> Gates = new List<Gate>();
-		public List<Trip> Trips = new List<Trip>();
-		public List<Passenger> Passengers = new List<Passenger>();
+        public List<Track> Tracks = new List<Track>();
+        public List<TrackSwitch> TrackSwitches = new List<TrackSwitch>();
+        public List<Station> Stations = new List<Station>();
+        public List<Train> Trains = new List<Train>();
+        public List<Gate> Gates = new List<Gate>();
+        public List<Trip> Trips = new List<Trip>();
+        public List<Passenger> Passengers = new List<Passenger>();
 
 
 
-		public  void LoadAllFiles()
-		{ 
-			
-		}
+        public void LoadAllFiles()
+        {
+            path = @"C:\Dev\the - railway - grupp - 3\Data\";
+            LoadFile(path, "Trains.txt");
+        }
 
-		public void LoadTrains()
-		{
-			using (var reader = new StreamReader(@"C:\Dev\the-railway-grupp-3\Data\trains.txt"))
-			{
-				int count = 0;
-				while (!reader.EndOfStream)
-				{
-					var line = reader.ReadLine();
-					var values = line.Split(',');
+        public void LoadFile(string path, string fileName)
+        {
+            string separator = GetSplitCharacter_NotPreDefined(path, fileName);
 
-					if (count == 0)
-					{
-						count++;
-						continue;
-					}
-					else
-					{
-						var train = new Train(int.Parse(values[0]), values[1], int.Parse(values[2]), bool.Parse(values[3]));
-						Trains.Add(train);
-					}
-				}
-			}
-		}
+            if (separator == "Empty")
+            {
+                throw new Exception("File is empty or unable to find the separator");
+            }
 
-		private string GetSplitCharacter()
-		{
-			int slash = 0;
-			int semicolon = 0;
-			int comma = 0;
-			int divider = 0;
-			int colon = 0;
+            using (var reader = new StreamReader(path + fileName))
+            {
+                int count = 0;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(separator);
 
+                    if (count == 0)
+                    {
+                        count++;
+                        continue;
+                    }
+                    else
+                    {
+                        var train = new Train(int.Parse(values[0]), values[1], int.Parse(values[2]), bool.Parse(values[3]));
+                        Trains.Add(train);
+                    }
+                }
+            }
+        }
 
-			return "";
-		}
+        public string GetSplitCharacter_NotPreDefined(string path, string fileName)
+        {
+            var possibleSeparators = new List<char>();
+            var score = new List<int>();
 
+            using (var sr = new StreamReader(path + fileName))
+            {
+                if (sr.EndOfStream) { return "Empty"; }
 
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    foreach (var character in line)
+                    {
+                        if (!Char.IsLetterOrDigit(character) && !Char.IsWhiteSpace(character))
+                        {
+                            var index = possibleSeparators.IndexOf(character);
+                            if (index < 0)
+                            {
+                                possibleSeparators.Add(character);
+                                score.Add(0);
+                            }
+                            else
+                            {
+                                score[index] += 1;
+                            }
+                        }
+                    }
+                }
+            }
 
-	}
+            var maxIndex = score.IndexOf(score.Max());
+            return possibleSeparators[maxIndex].ToString();
+        }
+
+    }
 }
